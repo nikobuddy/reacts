dbb6e9ad05c9d66d5c373b6bce19eb72fef26d60
nikobuddy <nisargalokhande@gmail.com>
2024-08-06 00:19:57 +0530

    Addeding temp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-400 to-indigo-600 text-white">
+      Hello Buddy
+    </div>
+  );
+}
+
+export default App;
